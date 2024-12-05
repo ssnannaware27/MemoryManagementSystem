3aa588fb8b0635c3112b00a7e0facd2894a6e19c
bha0910 <166706093+bha0910@users.noreply.github.com>
2024-12-05 21:52:34 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+#include <main.h>
+int main() {
+  int *arr = (int *)malloc(sizeof(int) * 8);
+  int i = 0;
+  for(i = 0; i < 8; i++) {
+    arr[i] = i;
+  }
+  for(i = 0; i < 8; i++) {
+    printf("%d", arr[i]);
+  }
+  free(arr);
+  return 0;
+}
