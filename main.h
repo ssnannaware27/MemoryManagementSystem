da7cf47a498007533560477f847ddcc558c1f7f2
Samruddhi Nannaware <samruddhi7259@gmail.com>
2024-12-05 22:03:59 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-void* malloc(size_t size);
-void *calloc(size_t number, size_t size);
-void free(void *ptr);
-void *realloc(void *p, size_t size);
+void* new_malloc(size_t size);
+void *new_calloc(size_t number, size_t size);
+void new_free(void *ptr);
+void *new_realloc(void *p, size_t size);
