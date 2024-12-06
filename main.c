9512a46eb2f02278ead0444e713ab7a15daa2800
bha0910 <166706093+bha0910@users.noreply.github.com>
2024-12-06 11:31:43 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+int main() {
+  int *arr = (int *)malloc(sizeof(int) * 8);
+  int i = 0;
+  for(i = 0; i < 8; i++) {
+    arr[i] = i;
+  }
+  for(i = 0; i < 8; i++) {
+    printf("%d ", arr[i]);
+  }
+  printf("\n");
+  free(arr);
+  return 0;
+}
